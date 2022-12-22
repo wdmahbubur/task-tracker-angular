4a87f56a6301064b085b02a44d6540d6e2908025
wdmahbubur <mrrohanc@gmail.com>
2022-12-22 12:52:52 +0600

    initial commit

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent {
+  @Input() task!:Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleRemainder : EventEmitter<Task> = new EventEmitter();
+   
+  faTimes = faTimes;
+  
+  onDelete(task: any){
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task:any){
+    this.onToggleRemainder.emit(task);
+  }
+
+}
